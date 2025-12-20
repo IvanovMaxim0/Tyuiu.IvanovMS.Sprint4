8752ea398915a2c9905a27d57f2454ab1b1779eb
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-20 16:14:14 +0500

    Task3

---
+++ Tyuiu.IvanovMS.Sprint4.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.IvanovMS.Sprint4.Task3.V12.Lib
+{
+    public class DataService : ISprint4Task3V12
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            int min = int.MaxValue;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j == 2 && array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint4.Task3.V12/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.IvanovMS.Sprint4.Task3.V12.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Иванов М. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* статическими значениями в диапазоне от 2 до 9. Найдите минимальный      *");
+        Console.WriteLine("* элемент третьем столбце массива.                                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        int[,] mas = new int[5, 5] { { 9, 7, 4, 9, 7 }, { 5, 2, 5, 7, 6 }, { 8, 3, 3, 5, 7 }, { 8, 4, 2, 4, 5 }, { 8, 8, 9, 5, 6 } };
+        int rows = mas.GetUpperBound(0) + 1;
+        int columns = mas.Length / rows;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mas[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int num = ds.Calculate(mas);
+        Console.WriteLine("Минимальный элемент в третьем столбце = " + num);
+    }
+}
