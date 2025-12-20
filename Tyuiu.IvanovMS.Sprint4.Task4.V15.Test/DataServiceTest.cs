ca8d6ede13d8e09247761d4f8b955b78a63f3448
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-20 16:46:00 +0500

    Task4

---
+++ Tyuiu.IvanovMS.Sprint4.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Tyuiu.IvanovMS.Sprint4.Task4.V15.Lib;
+namespace Tyuiu.IvanovMS.Sprint4.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] { { 4, 2, 5, 5, 3 }, { 4, 2, 5, 2, 2 }, { 3, 2, 4, 2, 4 }, { 3, 6, 4, 5, 4 }, { 2, 4, 6, 4, 2 } };
+            int res = ds.Calculate(matrix);
+            int wait = 2;
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint4.Task4.V15/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.IvanovMS.Sprint4.Task4.V15.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Двумерные массивы  (статический ввод)                                   *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 6. Найдите сумму нечетных   *");
+        Console.WriteLine("* элементов массива.                                                      *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,]  matr = new int[5, 5];
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine($"Введите значение элемента({i + 1},{j + 1})");
+                matr[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(matr[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(matr));
+    }
+}
