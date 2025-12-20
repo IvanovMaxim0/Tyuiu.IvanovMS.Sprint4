e639e915e62f7b2e348fe9ff4b5ad099e41b9963
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-20 19:19:20 +0500

    Task6

---
+++ Tyuiu.IvanovMS.Sprint4.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.IvanovMS.Sprint4.Task6.V27.Lib;
+namespace Tyuiu.IvanovMS.Sprint4.Task6.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            var figures = new string[] { "Квадрат", "Прямоугольник", "Круг", "Треугольник", "Пятиугольник", "Шестиугольник", "Восьмиугольник" };
+            int res = ds.Calculate(figures);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
